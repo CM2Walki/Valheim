c2e6d46bb6a2617d72728f394c721bf540869637
CM2Walki <walentinlamonos@googlemail.com>
2021-03-28 16:41:26 +0200

    Added support for valheim:plus

---
+++ buster-plus/Dockerfile
@@ -0,0 +1,5 @@
+FROM cm2network/valheim:base
+
+ENV VALHEIM_PLUS_VERSION 0.9.6
+
+# All other properties are inherited from the base image
--- etc/entry.sh
+++ etc/entry.sh
@@ -10,18 +10,37 @@ else
 	bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "$STEAMAPPDIR" +app_update "$STEAMAPPID" "${steamcmd_update_args[@]}" +quit
 fi
 
+# We assume that if the valheim plus config is missing, that this is a fresh container
+if [ ! -f "${STEAMAPPDIR}/start_server_bepinex.sh" ]; then
+	# Are we in a valheim plus container?
+	if [ ! -z "$VALHEIM_PLUS_VERSION" ]; then
+		wget --max-redirect=30 -qO- https://github.com/valheimPlus/ValheimPlus/releases/download/"${VALHEIM_PLUS_VERSION}"/UnixServer.tar.gz | tar xvzf - -C "${STEAMAPPDIR}"
+		chmod +x "${STEAMAPPDIR}/start_server_bepinex.sh"
+	fi
+fi
+
 cd "${STEAMAPPDIR}"
 
-"./valheim_server.x86_64" -batchmode \
-				-nographics \
-				-screen-width "${SCREEN_WIDTH}" \
-				-screen-height "${SCREEN_HEIGHT}" \
-				-screen-quality "${SCREEN_QUALITY}" \
-				-logFile "${SERVER_LOG_PATH}" \
-				-port "${SERVER_PORT}" \
-				-name "${SERVER_NAME}" \
-				-world "${SERVER_WORLD_NAME}" \
-				-password "${SERVER_PW}" \
-				-public "${SERVER_PUBLIC}" \
-				-savedir "${SERVER_SAVE_DIR}" \
-				{{ADDITIONAL_ARGS}}
+if [ ! -z "$VALHEIM_PLUS_VERSION" ]; then
+	# This is missing a couple of parameters, because the start_server_bepinex.sh doesn't support them
+	# Are we in a valheim plus container?
+	bash "start_server_bepinex.sh" -name "${SERVER_NAME}" \
+						-password "${SERVER_PW}" \
+						-port "${SERVER_PORT}" \
+						-world "${SERVER_WORLD_NAME}" \
+						-public "${SERVER_PUBLIC}"
+elif
+	"./valheim_server.x86_64" -batchmode \
+					-nographics \
+					-screen-width "${SCREEN_WIDTH}" \
+					-screen-height "${SCREEN_HEIGHT}" \
+					-screen-quality "${SCREEN_QUALITY}" \
+					-logFile "${SERVER_LOG_PATH}" \
+					-port "${SERVER_PORT}" \
+					-name "${SERVER_NAME}" \
+					-world "${SERVER_WORLD_NAME}" \
+					-password "${SERVER_PW}" \
+					-public "${SERVER_PUBLIC}" \
+					-savedir "${SERVER_SAVE_DIR}" \
+					{{ADDITIONAL_ARGS}}
+fi
